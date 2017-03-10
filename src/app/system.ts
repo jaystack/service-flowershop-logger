27b5bba50529784720782b3f9b9dde66a64b8be7
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2017-03-10 17:45:12 +0100

    update corpjs-system and -express

--- src/app/system.ts
+++ src/app/system.ts
@@ -1,4 +1,4 @@
-const {name} = require('../../package.json')
+const { name } = require('../../package.json')
 import System from 'corpjs-system'
 import Config from 'corpjs-config'
 import Endpoints from 'corpjs-endpoints'
@@ -6,20 +6,13 @@ import Logger from 'corpjs-logger'
 import Amqp from 'corpjs-amqp'
 import Consumer from './Consumer'
 
-const inDevEnv = process.env.NODE_ENV === 'dev'
-process.on('unhandledRejection', err => console.error(err))
-
-export default new System({ exitOnError: inDevEnv })
+export default new System({ name })
   .add('config', new Config()
     .add(() => Config.loaders.require({ path: 'config/default.js', watch: true }))
-    .add(() => Config.loaders.require({ path: 'config/test.js', watch: true }))
-  )
+    .add(() => Config.loaders.require({ path: 'config/test.js', watch: true })))
   .add('endpoints', Endpoints()).dependsOn({ component: 'config', source: 'endpoints', as: 'config' })
   .add('logger', Logger()).dependsOn({ component: 'config', source: 'logger', as: 'config' })
   .add('connection', Amqp.Connection()).dependsOn({ component: 'config', source: 'rabbit', as: 'config' }, 'endpoints')
   .add('channel', Amqp.Channel()).dependsOn('connection')
   .add('consumer', Consumer()).dependsOn('config', 'logger', 'channel')
-  .on('componentStart', (componentName: string) => console.log(`Started component: ${componentName}`))
-  .on('componentStop', (componentName: string) => console.log(`Stopped component: ${componentName}`))
-  .on('start', resources => console.log(`Started service: ${name}`))
-  .on('stop', (err, stopErr) => console.log(`Stopped service: ${name}`, err || '', stopErr || ''))
+  .logAllEvents()
